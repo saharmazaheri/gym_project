45b6a3129c9f7baf36e46073d89d524484b08c6e
saharmazaheri <saharmazaheri00@gmail.com>
2023-07-29 23:48:10 +0330

    add components footer

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
 <template>
   <div id="app">
-        <appHeader></appHeader>
-        <!-- Footer Start -->
-        <div class="footer wow fadeIn" data-wow-delay="0.3s">
-            <div class="container-fluid">
-                <div class="container">
-                    <div class="footer-info">
-                        <a href="index.html" class="footer-logo">H<span>ero</span> fit</a>
-                        <h3>123 Street, New York, USA</h3>
-                        <div class="footer-menu">
-                            <p>+012 345 67890</p>
-                            <p>info@example.com</p>
-                        </div>
-                        <div class="footer-social">
-                            <a href=""><i class="fab fa-twitter"></i></a>
-                            <a href=""><i class="fab fa-facebook-f"></i></a>
-                            <a href=""><i class="fab fa-youtube"></i></a>
-                            <a href=""><i class="fab fa-instagram"></i></a>
-                            <a href=""><i class="fab fa-linkedin-in"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="container copyright">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <p>&copy; <a href="#">Your Site Name</a>, All Right Reserved.</p>
-                        </div>
-                        <div class="col-md-6">
-                            <p>Designed By <a href="https://htmlcodex.com">HTML Codex</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Footer End -->
+    <appHeader />
+    <appFooter />
   </div>
 </template>
 
 <script>
-import Header from './pages/shared/Header.vue';
+import Header from './pages/shared/Header.vue'
+import Footer from './pages/shared/Footer.vue'
 export default {
-  components:{
-    appHeader:Header
+  components: {
+    appHeader: Header,
+    appFooter: Footer
   }
 }
 </script>
