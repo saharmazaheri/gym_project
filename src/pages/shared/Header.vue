ab127b9d907c107fe7da688cea919e54bab79506
saharmazaheri <saharmazaheri00@gmail.com>
2023-07-29 23:40:51 +0330

    add component header

---
+++ src/pages/shared/Header.vue
@@ -0,0 +1,95 @@
+<template>
+    <header>
+        <!-- Top Bar Start -->
+        <div class="top-bar d-none d-md-block">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="top-bar-left">
+                            <div class="text">
+                                <i class="far fa-clock"></i>
+                                <h2>8:00 - 9:00</h2>
+                                <p>شنبه - پنجشنبه</p>
+                            </div>
+                            <div class="text">
+                                <i class="fa fa-phone-alt"></i>
+                                <h2>+123 456 7890</h2>
+                                <p>For Appointment</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="top-bar-right">
+                            <div class="social">
+                                <a href=""><i class="fab fa-twitter"></i></a>
+                                <a href=""><i class="fab fa-facebook-f"></i></a>
+                                <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                <a href=""><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Top Bar End -->
+
+        <!-- Nav Bar Start -->
+        <div class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a href="index.html" class="navbar-brand">H<span>ero</span> fit</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto">
+                        <a href="index.html" class="nav-item nav-link active">خانه</a>
+                        <a href="about.html" class="nav-item nav-link">درباره‌ی ما</a>
+                        <a href="comments.html" class="nav-item nav-link">دیدگاه‌ها</a>
+                        <a href="service.html" class="nav-item nav-link">کافی شاپ</a>
+                        <a href="class.html" class="nav-item nav-link">دوره‌ها</a>
+                        <a href="trainer.html" class="nav-item nav-link">مربی‌ها</a>
+                        <a href="" class="nav-item nav-link">Pose</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Blog</a>
+                            <div class="dropdown-menu">
+                                <a href="" class="dropdown-item">Blog Grid</a>
+                                <a href="" class="dropdown-item">Blog Detail</a>
+                            </div>
+                        </div>
+                        <a href="resume.html" class="nav-item nav-link">رزومه</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Nav Bar End -->
+
+
+        <!-- Hero Start -->
+        <div class="hero">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-sm-12 col-md-6">
+                        <div class="hero-text">
+                            <h1>Lorem ipsum dolor sit amet</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet elit. Phasell nec pretum mi. Curabi ornare velit non. Aliqua
+                                metus tortor auctor quis sem.
+                            </p>
+                            <div class="hero-btn">
+                                <a class="btn" href="login.html">ورود</a>
+                                <a class="btn" href="signup.html">ثبت نام</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 d-none d-md-block">
+                        <div class="hero-image">
+                            <img src="img/hero.png" alt="Hero Image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hero End -->
+    </header>
+</template>
