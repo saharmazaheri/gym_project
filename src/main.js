eb076e986e5f0af7e31e404e4458afb8aaaa782d
saharmazaheri <saharmazaheri00@gmail.com>
2023-07-30 23:17:11 +0330

    add mode

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 
-import {Routes} from './Routes'
+import { Routes } from './Routes'
 
-Vue.use(VueRouter); 
+Vue.use(VueRouter);
 
-const router= new VueRouter({ 
-  routes:Routes
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history'
 });
 
 new Vue({
