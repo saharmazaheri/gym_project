67ec4b68a69e910ae1ebee4bb269dc9a6503a0f9
saharmazaheri <saharmazaheri00@gmail.com>
2023-07-30 19:28:28 +0330

    remove the tag div

--- src/pages/Index/Index.vue
+++ src/pages/Index/Index.vue
@@ -1,29 +1,29 @@
 <template>
-    <div>
-        <!-- About Start -->
-        <div class="about wow fadeInUp " data-wow-delay="0.1s">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-5 col-md-6">
-                        <div class="about-img">
-                            <img src="img/about.png" alt="Image">
-                        </div>
+    <!-- About Start -->
+    <div class="about wow fadeInUp " data-wow-delay="0.1s">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 col-md-6">
+                    <div class="about-img">
+                        <img src="img/about.png" alt="Image">
                     </div>
-                    <div class="col-lg-7 col-md-6">
-                        <div class="section-header text-center">
-                            <p>About Hero</p>
-                            <h2>به هیرو خوش آمدید</h2>
-                        </div>
-                        <div class="about-text">
-                            <p dir="rtl">
-                                تناسب اندام به معنای بهتر بودن از شخص دیگر نیست ... بلکه بهتر بودن از گذشته است. تیم بزرگ هیرو فیت با در دست داشتن متخصصین با تجربه و دلسوز به شما عزیزان کمک خواهد کرد تا از گذشته خود پیشی بگیرید و در این راه در دام سود جویان گرفتار نشوید.
-                            </p>
-                            <a class="btn" href="about.html"> ادامه‌ی مطلب</a>
-                        </div>
+                </div>
+                <div class="col-lg-7 col-md-6">
+                    <div class="section-header text-center">
+                        <p>About Hero</p>
+                        <h2>به هیرو خوش آمدید</h2>
+                    </div>
+                    <div class="about-text">
+                        <p dir="rtl">
+                            تناسب اندام به معنای بهتر بودن از شخص دیگر نیست ... بلکه بهتر بودن از گذشته است. تیم بزرگ هیرو
+                            فیت با در دست داشتن متخصصین با تجربه و دلسوز به شما عزیزان کمک خواهد کرد تا از گذشته خود پیشی
+                            بگیرید و در این راه در دام سود جویان گرفتار نشوید.
+                        </p>
+                        <a class="btn" href="about.html"> ادامه‌ی مطلب</a>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- About End -->
     </div>
+    <!-- About End -->
 </template>
