446a39d30ce44fff0a2aad2c67dcc57205828188
saharmazaheri <saharmazaheri00@gmail.com>
2023-07-30 21:24:59 +0330

    import router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import {Routes} from './Routes'
+
+Vue.use(VueRouter); 
+
+const router= new VueRouter({ 
+  routes:Routes
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
