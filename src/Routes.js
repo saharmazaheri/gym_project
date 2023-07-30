ed327452b8e20fd036c2418e7e15ff38031a79a6
saharmazaheri <saharmazaheri00@gmail.com>
2023-07-30 21:43:00 +0330

    add the AboutUs component to the Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,13 @@
 import Index from './pages/Index/Index.vue'
+import AboutUs from './pages/AboutUs/About.vue'
 
 export const Routes = [
     {
         path: '/',
         component: Index
+    },
+    {
+        path: '/AboutUs',
+        component: AboutUs
     }
 ];
