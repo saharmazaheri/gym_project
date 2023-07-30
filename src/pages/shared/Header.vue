31b791d38df3ba4e044b9905cb78b88ab01276d3
saharmazaheri <saharmazaheri00@gmail.com>
2023-07-30 23:34:22 +0330

    add router-link UserOpinion

--- src/pages/shared/Header.vue
+++ src/pages/shared/Header.vue
@@ -44,8 +44,8 @@
                 <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                     <div class="navbar-nav ml-auto">
                         <router-link to="/" active-class="active" exact class="nav-item nav-link">خانه</router-link>
-                        <router-link to="/AboutUs" active-class="active" class="nav-item nav-link">درباره‌ی ما</router-link>
-                        <a href="comments.html" class="nav-item nav-link">دیدگاه‌ها</a>
+                        <router-link to="/AboutUs" active-class="active" class="nav-item nav-link">درباره‌ما</router-link>
+                        <router-link to="/UserOpinion" active-class="active" class="nav-item nav-link">دیدگاه‌ها</router-link>
                         <a href="service.html" class="nav-item nav-link">کافی شاپ</a>
                         <a href="class.html" class="nav-item nav-link">دوره‌ها</a>
                         <a href="trainer.html" class="nav-item nav-link">مربی‌ها</a>
