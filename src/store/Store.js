71153868218313c3e14db0f49f5e4b8a8325f1ea
saharmazaheri <saharmazaheri00@gmail.com>
2023-08-01 12:56:44 +0330

    add import Courses.js

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,8 +1,13 @@
 import Vue from "vue"
 import Vuex from 'vuex'
 
+import Courses from "./Modules/Courses"
+
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {}
+    state: {},
+    modules:{
+        Courses
+    }
 })
