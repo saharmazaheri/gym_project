2350a13f5514614f822bfe5231e54512441ba74a
saharmazaheri <saharmazaheri00@gmail.com>
2023-08-01 12:56:12 +0330

    remove additional codes, implementation using view

--- src/pages/Courses/Courses.vue
+++ src/pages/Courses/Courses.vue
@@ -1,261 +1,71 @@
 <template>
-    <viv>
+    <div>
         <!-- Page Header Start -->
-    <div class="page-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2>دوره‌ها</h2>
-                </div>
-                <div class="col-12">
-                    <a href="">خانه</a>
-                    <a href="">دوره‌ها</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Page Header End -->
-
-
-    <!-- Class Start -->
-    <div class="service">
-        <div class="container">
-            <div class="class">
-                <div class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
-                    <p>دوره‌ها</p>
-                    <h2>مشخصات دوره‌ها</h2>
-                </div>
-                <div class="row" dir="rtl">
+        <div class="page-header">
+            <div class="container">
+                <div class="row">
                     <div class="col-12">
-                        <ul id="class-filter">
-                            <li data-filter="*" class="filter-active">همه دوره‌ها</li>
-                            <li data-filter=".filter-1">عمومی</li>
-                            <li data-filter=".filter-2">خصوصی</li>
-                            <li data-filter=".filter-3">آزاد</li>
-                        </ul>
+                        <h2>دوره‌ها</h2>
+                    </div>
+                    <div class="col-12">
+                        <a href="">خانه</a>
+                        <a href="">دوره‌ها</a>
                     </div>
                 </div>
             </div>
-            <div class="row class-container">
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-1 wow fadeInUp" data-wow-delay="0.0s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout"></i>
-                        </div>
-                        <h3>عمومی: 8 جلسه یک ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
+        </div>
+        <!-- Page Header End -->
+
+
+        <!-- Class Start -->
+        <div class="service">
+            <div class="container">
+                <div class="class">
+                    <div class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
+                        <p>دوره‌ها</p>
+                        <h2>مشخصات دوره‌ها</h2>
+                    </div>
+                    <div class="row" dir="rtl">
+                        <div class="col-12">
+                            <ul id="class-filter">
+                                <li data-filter="*" class="filter-active">همه دوره‌ها</li>
+                                <li data-filter=".filter-1">عمومی</li>
+                                <li data-filter=".filter-2">خصوصی</li>
+                                <li data-filter=".filter-3">آزاد</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-1 wow fadeInUp" data-wow-delay="0.2s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout-1"></i>
-                        </div>
-                        <h3>عمومی: 12 جلسه یک ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-1 wow fadeInUp" data-wow-delay="0.4s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout-2"></i>
-                        </div>
-                        <h3>عمومی: 16 جلسه یک ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-1 wow fadeInUp" data-wow-delay="0.6s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout-3"></i>
-                        </div>
-                        <h3>عمومی: 12 جلسه دو ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-1 wow fadeInUp" data-wow-delay="0.8s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout-4"></i>
-                        </div>
-                        <h3>عمومی: 16 جلسه دو ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-1 wow fadeInUp" data-wow-delay="1s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-yoga-pose"></i>
-                        </div>
-                        <h3>عمومی: 24 جلسه سه ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-2 wow fadeInUp" data-wow-delay="0.0s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout"></i>
-                        </div>
-                        <h3>خصوصی: 8 جلسه یک ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-2 wow fadeInUp" data-wow-delay="0.2s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout-1"></i>
-                        </div>
-                        <h3>خصوصی: 12 جلسه یک ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-2 wow fadeInUp" data-wow-delay="0.4s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout-2"></i>
-                        </div>
-                        <h3>خصوصی: 16 جلسه یک ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-2 wow fadeInUp" data-wow-delay="0.6s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout-3"></i>
-                        </div>
-                        <h3>خصوصی: 12 جلسه دو ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-2 wow fadeInUp" data-wow-delay="0.8s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-workout-4"></i>
-                        </div>
-                        <h3>خصوصی: 16 جلسه دو ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-2 wow fadeInUp" data-wow-delay="1s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-yoga-pose"></i>
-                        </div>
-                        <h3>خصوصی: 24 جلسه سه ماه</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-3 wow fadeInUp" data-wow-delay="1s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-yoga-pose"></i>
-                        </div>
-                        <h3>آزاد: تک جلسه عمومی</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 class-item filter-3 wow fadeInUp" data-wow-delay="1s">
-                    <div class="service-item">
-                        <div class="service-icon">
-                            <i class="flaticon-yoga-pose"></i>
-                        </div>
-                        <h3>آزاد: تک جلسه خصوصی</h3>
-                        <p>
-                            250,000 تومان
-                        </p>
-                        <div class="class-meta icon">
-                            <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
-                            <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
+                <div class="row class-container">
+                    <div class="col-lg-4 col-md-6 col-sm-12 class-item wow fadeInUp" :class="item.fillter" data-wow-delay="0.0s"
+                        v-for="item in courses">
+                        <div class="service-item">
+                            <div class="service-icon">
+                                <i :class="item.imge"></i>
+                            </div>
+                            <h3>{{ item.type }}</h3>
+                            <p>{{ item.numberOfSessions }}</p>
+                            <p>
+                                {{ item.price }}</p>
+                            <div class="class-meta icon">
+                                <p>شنبه، چهارشنبه <i class="far fa-calendar-alt"></i></p>
+                                <p>8:00 - 12:00 <i class="far fa-clock"></i></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- Class End -->
     </div>
-    <!-- Class End -->
-    </viv>
 </template>
 
 <script>
 export default {
-    
+    computed: {
+        courses() {
+            return this.$store.getters.GetCourses;
+        }
+    }
 }
 </script>
