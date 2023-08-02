763719c288d651a7986fed4beaab3daa6162f9da
saharmazaheri <saharmazaheri00@gmail.com>
2023-08-02 11:41:40 +0330

    create component CoffeShop.vue

---
+++ src/pages/CoffeeShop.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+        <!-- Page Header Start -->
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>کافی شاپ</h2>
+                </div>
+                <div class="col-12">
+                    <a href="">خانه</a>
+                    <a href="">کافی شاپ</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
+    <!-- Service Start -->
+    <div class="service" dir="rtl">
+        <div class="container">
+            <div class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
+                <p>منو کافه</p>
+                <h2>مشاهده منو کافه</h2>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.0s">
+                    <a href="index.html">
+                        <div class="service-item">
+                            <div class="service-icoon">
+                                <img src="img/coffee/gh-1.PNG" alt="Image">
+                            </div>
+                            <h3>اسپرسو</h3>
+                            <p>69.000 تومان</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.0s">
+                    <a href="index.html">
+                        <div class="service-item">
+                            <div class="service-icoon">
+                                <img src="img/coffee/gh-2.PNG" alt="Image">
+                            </div>
+                            <h3>اسپرسو کن پانا</h3>
+                            <p>اسپرسو، خامه</p>
+                            <p>74.000 تومان</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.0s">
+                    <a href="index.html">
+                        <div class="service-item">
+                            <div class="service-icoon">
+                                <img src="img/coffee/gh-3.PNG" alt="Image">
+                            </div>
+                            <h3>اسپرسو ماکیاتو</h3>
+                            <p>اسپرسو، چند قطره شیر، یک لکه فوم شیر</p>
+                            <p>71.000 تومان</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.0s">
+                    <a href="index.html">
+                        <div class="service-item">
+                            <div class="service-icoon">
+                                <img src="img/coffee/gh-1.PNG" alt="Image">
+                            </div>
+                            <h3>اسپرسو</h3>
+                            <p>69.000 تومان</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.0s">
+                    <a href="index.html">
+                        <div class="service-item">
+                            <div class="service-icoon">
+                                <img src="img/coffee/gh-1.PNG" alt="Image">
+                            </div>
+                            <h3>اسپرسو</h3>
+                            <p>69.000 تومان</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.0s">
+                    <a href="index.html">
+                        <div class="service-item">
+                            <div class="service-icoon">
+                                <img src="img/coffee/gh-1.PNG" alt="Image">
+                            </div>
+                            <h3>اسپرسو</h3>
+                            <p>69.000 تومان</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Service End -->
+    </div>
+</template>
