924f31a2082b74396c974e65c6a17f16ae3403c6
saharmazaheri <saharmazaheri00@gmail.com>
2023-08-02 10:28:32 +0330

    rename components

---
+++ src/pages/About.vue
@@ -0,0 +1,118 @@
+<template>
+    <div>
+        <!-- Page Header Start -->
+        <div class="page-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>درباره‌ی ما</h2>
+                    </div>
+                    <div class="col-12" >
+                        <router-link to="/">خانه</router-link>
+                        <router-link to="/AboutUs">درباره‌ی ما</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+
+        <!-- About Start -->
+        <div class="about wow fadeInUp text-center" data-wow-delay="0.1s" dir="rtl">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-5 col-md-6">
+                        <div class="about-img">
+                            <img src="img/about.png" alt="Image">
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-6">
+                        <div class="section-header text">
+                            <p>About Hero</p>
+                            <h2>به هیرو خوش آمدید</h2>
+                        </div>
+                        <div class="about-text ">
+                            <p>
+                                تناسب اندام به معنای بهتر بودن از شخص دیگر نیست ... بلکه بهتر بودن از گذشته است. تیم بزرگ هیرو فیت با در دست داشتن متخصصین با تجربه و دلسوز به شما عزیزان کمک خواهد کرد تا از گذشته خود پیشی بگیرید و در این راه در دام سود جویان گرفتار نشوید.
+                            </p>
+                            <p>
+                                مجموعه ورزشی هیرو بزرگترین و مجهزترین مجموعه ورزشی با هدف ارائه خدمات ورزشی به صورت حرفه‌ای و در سطح استانداردهای جهانی سبکی نوین در عرصه باشگاه‌های بدنسازی ایجاد کرده است. به طوری که این مجموعه با شروع فعالیت خود مورد استقبال بی‌نظیر اهالی ورزش، هنرمندان و سایر علاقه‌مندان به فعالیت‌های ورزشی قرار گرفت.
+مجموعه ورزشی هیرو واقع در قلب پايتخت و در منطقه اداری تجاری با امكان ارائه خدمات لوكس و حرفه‌ای، ورزش و تندرستی را به علاقه‌مندان فراهم می‌نمايد. هیرو  با مساحت ۵۰۰ متر و طول سقف ۵ متر و سیستم تهویه روز دنیا، تمام تلاش خود را برای بهتر بودن کرده است، تا مربيان حرفه‌ای و تحصیلکرده، بهترین طراحی تمرین با متد روز دنیا و … را برای شما در نظر بگیرند.
+ 
+                            </p>
+                            <p>
+                                هیرو به معنای قهرمان و برگرفته از انرژی که از نامش پيداست، خدمات ورزشی را برای تمامی سنین و سلیقه‌ها فراهم می‌نماید .
+                            </p>
+                            <p >
+                                همچنین کافی‌شاپ بسیار شیک برای گرفتن پروتئین و رژیم غذایی رایگان از مربیان در قبل و بعد از ورزش خیال آسوده را برای شما فراهم نموده است . 
+هیرو این امکان را بوجود آورده است که ورزشکاران در هر زمان مورد نظر خود به باشگاه مراجعه نموده و از خدمات و امکانات سطح بالا با استانداردهای روز دنيا استفاده نمايند.
+داشتن پارکینگ و پارکبان نیز از دیگر مزایای این باشگاه می باشد.
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="a-text">
+            <div class="container">
+                <div class="atext text-right" >
+                    <br>
+                    <h2>چرا باشگاه ورزشی هیرو؟</h2>
+                    <p>
+                    .ما با داشتن تیم مجرب و حرفه ای جهت ارایه بهترین خدمات در خدمت شما خواهیم بود. تیم ما متشکل از مربیان است که همگی از فارغ التحصیلان رشته تربیت بدنی؛ وبا تجربه فراوان در رشته های مختلف ورزشی میباشند؛ و با داشتن دکترتغذیه در باشگاه جهت ارایه رژیم غذایی مناسب به شما عزیزان و همچنین راهنمایی دکتر عزیزمان آماده ارایه سرویس مناسب غذایی در بخش کافی شاپ هستیم
+                    </p>
+                    <h2>:مربیان با تجربه</h2>
+                    <p>
+                    .تیم مشاوره و ثبت نام با داشتن مدرک ورزشی مرتبط؛ هنگام ثبت نام شما را در انتخاب  کلاس و دوره مناسبت راهنمایی خواهند کرد
+                    </p>
+                    <h2>:امکانات مدرن ورزشی و تناسب اندام</h2>
+                    <p>
+                    .تمام تلاش ما بر این است  که محیطی کاملا شاد و با انگیره بالا با استانداردهای روز؛ برای شما ورزشکاران گرامی فراهم نماییم
+                    </p>
+                </div>
+            </div>
+            </div>
+        <!-- About End -->
+
+
+        <!-- Contact Start -->
+        <div class="contact">
+            <div class="container">
+                <div class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
+                    <p>Get In Touch</p>
+                    <h2>For Any Query</h2>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-md-4 contact-item wow zoomIn" data-wow-delay="0.2s">
+                                <i class="fa fa-map-marker-alt"></i>
+                                <div class="contact-text">
+                                    <h2>Location</h2>
+                                    <p>123 Street, New York, USA</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4 contact-item wow zoomIn" data-wow-delay="0.4s">
+                                <i class="fa fa-phone-alt"></i>
+                                <div class="contact-text">
+                                    <h2>Phone</h2>
+                                    <p>+012 345 67890</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4 contact-item wow zoomIn" data-wow-delay="0.6s">
+                                <i class="far fa-envelope"></i>
+                                <div class="contact-text">
+                                    <h2>Email</h2>
+                                    <p>info@example.com</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact End -->
+    </div>
+</template>
---
+++ src/pages/Index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <!-- Hero Start -->
+        <div class="hero">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-sm-12 col-md-6">
+                        <div class="hero-text">
+                            <h1>Lorem ipsum dolor sit amet</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet elit. Phasell nec pretum mi. Curabi ornare velit non. Aliqua
+                                metus tortor auctor quis sem.
+                            </p>
+                            <div class="hero-btn">
+                                <a class="btn" href="login.html">ورود</a>
+                                <a class="btn" href="signup.html">ثبت نام</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 d-none d-md-block">
+                        <div class="hero-image">
+                            <img src="img/hero.png" alt="Hero Image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hero End -->
+        <!-- About Start -->
+        <div class="about wow fadeInUp " data-wow-delay="0.1s">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-5 col-md-6">
+                        <div class="about-img">
+                            <img src="img/about.png" alt="Image">
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-6">
+                        <div class="section-header text-center">
+                            <p>About Hero</p>
+                            <h2>به هیرو خوش آمدید</h2>
+                        </div>
+                        <div class="about-text">
+                            <p dir="rtl">
+                                تناسب اندام به معنای بهتر بودن از شخص دیگر نیست ... بلکه بهتر بودن از گذشته است. تیم بزرگ
+                                هیرو
+                                فیت با در دست داشتن متخصصین با تجربه و دلسوز به شما عزیزان کمک خواهد کرد تا از گذشته خود
+                                پیشی
+                                بگیرید و در این راه در دام سود جویان گرفتار نشوید.
+                            </p>
+                            <a class="btn" href="about.html"> ادامه‌ی مطلب</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+    </div>
+</template>
