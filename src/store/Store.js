6e8445e006326e0263e475a61c740ec290fc4544
saharmazaheri <saharmazaheri00@gmail.com>
2023-08-02 10:27:29 +0330

    import Trainers.js to Store.js

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,12 +2,14 @@ import Vue from "vue"
 import Vuex from 'vuex'
 
 import Courses from "./Modules/Courses"
+import Trainers from "./Modules/Trainers"
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {},
     modules:{
-        Courses
+        Courses,
+        Trainers
     }
-})
+});
