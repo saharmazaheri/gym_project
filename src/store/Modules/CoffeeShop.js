04a2ca3dbf7472a6db52187a097e14db1f0d07c9
saharmazaheri <saharmazaheri00@gmail.com>
2023-08-07 17:28:32 +0330

    edit codes

--- src/store/Modules/CoffeeShop.js
+++ src/store/Modules/CoffeeShop.js
@@ -4,83 +4,105 @@ const state = {
             imge: require('../../assets/img/coffee/gh-1.png'),
             title: "اسپرسو",
             comments: "اسپرسو",
-            price: "69.000 تومان"
+            price: "69000",
+            id:"0"
         },
         {
             imge: require('../../assets/img/coffee/gh-2.png'),
             title: "اسپرسو کن پانا",
             comments: "اسپرسو، خامه",
-            price: "74.000 تومان"
+            price: "74000",
+            id:"1"
         },
         {
             imge: require('../../assets/img/coffee/gh-3.png'),
             title: "اسپرسو ماکیاتو",
             comments: "اسپرسو، چند قطره شیر، یک لکه فوم شیر",
-            price: "71.000 تومان"
+            price: "71000",
+            id:"2"
         },
         {
             imge: require('../../assets/img/coffee/sh-b.png'),
             title: "شیربادام عسل دارچین",
             comments: "شیربادام،عسل،دارچین",
-            price: "123.000 تومان"
+            price: "123000",
+            id:"3"
         },
         {
             imge: require('../../assets/img/coffee/sh-f.png'),
             title: "شیرفندق عسل دارچین",
             comments: "شیرفندق،عسل،دارچین",
-            price: "123.000 تومان"
+            price: "123000",
+            id:"4"
         },
         {
             imge: require('../../assets/img/coffee/sh-n.png'),
             title: "شیرنارگیل عسل دارچین",
             comments: "شیرنارگیل،عسل،دارچین",
-            price: "112.000 تومان"
+            price: "112000",
+            id:"5"
         },
         {
             imge: require('../../assets/img/coffee/i-a.png'),
             title: "آیس آمریکانو",
             comments: "آمریکانو، یخ",
-            price: "69.000 تومان"
+            price: "69000",
+            id:"6"
         },
         {
             imge: require('../../assets/img/coffee/i-l.png'),
             title: "آیس لاته",
             comments: "اسپرسو، شیر، یخ",
-            price: "84.000 تومان"
+            price: "84000",
+            id:"7"
         },
         {
             imge: require('../../assets/img/coffee/i-m.png'),
             title: "آیس موکا",
             comments: "شکلات، اسپرسو، شیر، یخ",
-            price: "89.000 تومان"
+            price: "89000",
+            id:"8"
         },
         {
             imge: require('../../assets/img/coffee/c-z.png'),
             title: "کوکی زنجبیل عسل (عددی)",
             comments: "یک عدد کوکی زنجبیل",
-            price: "39.000 تومان"
+            price: "39000",
+            id:"9"
         },
         {
             imge: require('../../assets/img/coffee/c-b.png'),
             title: "کوکی براونی چاکلت (عددی)",
             comments: "یک عدد کوکی براونی چاکلت",
-            price: "39.000 تومان"
+            price: "39000",
+            id:"10"
         },
         {
             imge: require('../../assets/img/coffee/c-k.png'),
             title: "کوکی بادام کرنبری (عددی)",
             comments: "کوکی بادام کرنبری)",
-            price: "39.000 تومان"
+            price: "39000",
+            id:"11"
         },
 
-    ]
+    ],
+    shopcart:[],
+    Total:[],
+    
 };
 
 const getters = {
     GetCoffee(state) {
         return state.coffee;
-    }
-
+    },
+    Getshopcart(state){
+        return state.shopcart;
+    },
+    GetTotal(state){
+        return state.Total;
+    },
+   
+    
 };
 const mutations = {
 
